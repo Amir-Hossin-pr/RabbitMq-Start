886431a6edd02737131b2c72696e7398d46fb0c3
amir hossin <65053996+Amir-Hossin-pr@users.noreply.github.com>
2022-03-31 06:50:52 -0700

    create sender message

---
+++ RabbitMQ/Erth/Program.cs
@@ -0,0 +1,23 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using static System.Console;
+using static System.Text.Encoding;
+
+WriteLine("Hello From Erth");
+
+ConnectionFactory factory = new() { HostName = "localhost" };
+IConnection? connection = factory.CreateConnection();
+IModel channel = connection.CreateModel();
+
+channel.QueueDeclare("inbox", true, false, false, null);
+
+string? message = " ";
+while (message != string.Empty)
+{
+    Write("Write Your Message : ");
+    message = ReadLine();
+
+    byte[] messageBytes = UTF8.GetBytes(message ?? "");
+    channel.BasicPublish("", "inbox", null, messageBytes);
+    WriteLine($"Erth Send : [{message}]");
+}
