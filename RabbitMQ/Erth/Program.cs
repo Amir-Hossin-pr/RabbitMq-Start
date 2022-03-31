ff52ef7521477dcd438a586f42a3013abe17e4e4
amir hossin <65053996+Amir-Hossin-pr@users.noreply.github.com>
2022-03-31 09:00:53 -0700

    create header exchange and send message

--- RabbitMQ/Erth/Program.cs
+++ RabbitMQ/Erth/Program.cs
@@ -3,21 +3,48 @@ using RabbitMQ.Client.Events;
 using static System.Console;
 using static System.Text.Encoding;
 
-WriteLine("Hello From Erth");
+WriteLine("Hello From Earth");
 
 ConnectionFactory factory = new() { HostName = "localhost" };
 IConnection? connection = factory.CreateConnection();
 IModel channel = connection.CreateModel();
 
+Dictionary<string, object> headers = new()
+{
+    ["x-match"] = "any",
+    ["format"] = "bmp"
+};
+
+Dictionary<string, object> headers2 = new()
+{
+    ["x-match"] = "any",
+    ["format"] = "mkv"
+};
+
+
+channel.ExchangeDeclare("format", "headers", true, false, null);
 channel.QueueDeclare("inbox", true, false, false, null);
+channel.QueueDeclare("inboxMkv", true, false, false, null);
+
+channel.QueueBind("inbox", "format", "", headers);
+channel.QueueBind("inboxMkv", "format", "", headers2);
 
 string? message = " ";
 while (message != string.Empty)
 {
     Write("Write Your Message : ");
     message = ReadLine();
+    Write("What is your format? : ");
+    string? format = ReadLine();
+   
+    IBasicProperties? properties = channel.CreateBasicProperties();
+    properties.Headers = new Dictionary<string, object>
+    {
+        ["time"] = DateTime.Now.ToShortTimeString(),
+        ["format"] = format ?? "",
+    };
 
     byte[] messageBytes = UTF8.GetBytes(message ?? "");
-    channel.BasicPublish("", "inbox", null, messageBytes);
-    WriteLine($"Erth Send : [{message}]");
+    channel.BasicPublish("format", format, properties, messageBytes);
+    WriteLine($"Earth Send : [{message}]");
 }
