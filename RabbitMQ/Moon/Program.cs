4ea448a22c933a9ad3dd763b72e8a356cfd295a0
amir hossin <65053996+Amir-Hossin-pr@users.noreply.github.com>
2022-03-31 09:01:08 -0700

    create thread for tow queue

--- RabbitMQ/Moon/Program.cs
+++ RabbitMQ/Moon/Program.cs
@@ -8,20 +8,55 @@ WriteLine("Hello From Moon");
 
 ConnectionFactory factory = new() { HostName = "localhost" };
 IConnection? connection = factory.CreateConnection();
-IModel channel = connection.CreateModel();
 
-channel.QueueDeclare("inbox", true, false, false, null);
 
-EventingBasicConsumer consumer = new(channel);
-consumer.Received += Received;
-
-static void Received(object? sender, BasicDeliverEventArgs eventArgs)
+Thread t1 = new(() =>
 {
-    byte[] messageBytes = eventArgs.Body.ToArray();
-    var message = UTF8.GetString(messageBytes);
-    WriteLine($"Erth Sended : [{message}]");
-}
+    IModel channel = connection.CreateModel();
+    channel.QueueDeclare("inbox", true, false, false, null);
 
-channel.BasicConsume("inbox", false, consumer);
+    EventingBasicConsumer consumer = new(channel);
+    consumer.Received += Received;
+
+    static void Received(object? sender, BasicDeliverEventArgs eventArgs)
+    {
+        WriteLine("Inbox Queue");
+        byte[] messageBytes = eventArgs.Body.ToArray();
+        var message = UTF8.GetString(messageBytes);
+        WriteLine($"Earth Sended : [{message}],Routing Key : [{eventArgs.RoutingKey}], Exchange : [{eventArgs.Exchange}]");
+        foreach (var header in eventArgs.BasicProperties.Headers)
+        {
+            WriteLine($"Header : [{header.Key}] : [{UTF8.GetString((byte[])header.Value)}]");
+        }
+    }
+
+    channel.BasicConsume("inbox", false, consumer);
+});
+
+Thread t2 = new(() =>
+{
+    IModel channel = connection.CreateModel();
+    channel.QueueDeclare("inboxMkv", true, false, false, null);
+
+    EventingBasicConsumer consumer = new(channel);
+    consumer.Received += Received;
+
+    static void Received(object? sender, BasicDeliverEventArgs eventArgs)
+    {
+        WriteLine("InboxMkv Queue");
+        byte[] messageBytes = eventArgs.Body.ToArray();
+        var message = UTF8.GetString(messageBytes);
+        WriteLine($"Earth Sended : [{message}],Routing Key : [{eventArgs.RoutingKey}], Exchange : [{eventArgs.Exchange}]");
+        foreach (var header in eventArgs.BasicProperties.Headers)
+        {
+            WriteLine($"Header : [{header.Key}] : [{UTF8.GetString((byte[])header.Value)}]");
+        }
+    }
+
+    channel.BasicConsume("inboxMkv", false, consumer);
+});
+
+t1.Start();
+t2.Start();
 
 ReadKey();
