9350560124eadbea275f5b1617aa63ad1d49e3f6
amir hossin <65053996+Amir-Hossin-pr@users.noreply.github.com>
2022-03-31 07:22:45 -0700

    create exchange and queue bindings

--- RabbitMQ/Erth/Program.cs
+++ RabbitMQ/Erth/Program.cs
@@ -10,14 +10,27 @@ IConnection? connection = factory.CreateConnection();
 IModel channel = connection.CreateModel();
 
 channel.QueueDeclare("inbox", true, false, false, null);
+channel.QueueDeclare("a", true, false, false, null);
+channel.QueueDeclare("b", true, false, false, null);
+channel.QueueDeclare("c", true, false, false, null);
+channel.ExchangeDeclare("contact", "direct", false, false, null);
+
+channel.QueueBind("inbox", "contact", "blue", null);
+channel.QueueBind("a", "contact", "blue", null);
+channel.QueueBind("b", "contact", "red", null);
+channel.QueueBind("c", "contact", "red", null);
 
 string? message = " ";
+string? key = "";
 while (message != string.Empty)
 {
     Write("Write Your Message : ");
     message = ReadLine();
 
+    Write("Enter Your Queue Key : ");
+    key = ReadLine();
+
     byte[] messageBytes = UTF8.GetBytes(message ?? "");
-    channel.BasicPublish("", "inbox", null, messageBytes);
-    WriteLine($"Erth Send : [{message}]");
+    channel.BasicPublish("contact", key, null, messageBytes);
+    WriteLine($"Earth Send : [{message}]");
 }
