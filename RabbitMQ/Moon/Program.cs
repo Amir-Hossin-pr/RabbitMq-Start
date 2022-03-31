4ed2e444c4bd4cdb457bb6100e20ef7611582a6a
amir hossin <65053996+Amir-Hossin-pr@users.noreply.github.com>
2022-03-31 06:51:05 -0700

    create Receiver message

---
+++ RabbitMQ/Moon/Program.cs
@@ -0,0 +1,27 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+using static System.Console;
+using static System.Text.Encoding;
+
+WriteLine("Hello From Moon");
+
+ConnectionFactory factory = new() { HostName = "localhost" };
+IConnection? connection = factory.CreateConnection();
+IModel channel = connection.CreateModel();
+
+channel.QueueDeclare("inbox", true, false, false, null);
+
+EventingBasicConsumer consumer = new(channel);
+consumer.Received += Received;
+
+static void Received(object? sender, BasicDeliverEventArgs eventArgs)
+{
+    byte[] messageBytes = eventArgs.Body.ToArray();
+    var message = UTF8.GetString(messageBytes);
+    WriteLine($"Erth Sended : [{message}]");
+}
+
+channel.BasicConsume("inbox", false, consumer);
+
+ReadKey();
